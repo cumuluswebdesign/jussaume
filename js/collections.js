c68ef182b3b8cc16b9a52934656bfb3f5e2b5d53
Francis Marineau <frank.marineau@gmail.com>
2013-06-23 13:30:34 -0400

    transparency and new hover state for collections

--- js/collections.js
+++ js/collections.js
@@ -1,25 +1,35 @@
-function emphasizeNav($navElement) {
+function emphasizeNav($navElement, doResize) {
 	$navElement.find('p').css('color', '#111');
-	$navElement.find('img').css({
-		'width': '100px',
-		'margin-top': '0'
-	});
+
+	if (doResize) {
+		$navElement.find('img').css({
+			'width': '100px',
+			'margin-top': '0'
+		});
+	}
+
+	$navElement.css('opacity', '1');
 }
 
-function deEmphasizeNav($navElement) {
+function deEmphasizeNav($navElement, doResize) {
 	$navElement.find('p').css('color', '#a6a6a6');
-	$navElement.find('img').css({
-		'width': '90px',
-		'margin-top': '5px'
-	});
+
+	if (doResize) {
+		$navElement.find('img').css({
+			'width': '90px',
+			'margin-top': '5px'
+		});
+	}
+
+	$navElement.css('opacity', '0.5');
 }
 
 //Collection nav hover states
 $('.collection-item').hover(function() {
-	emphasizeNav($(this));
+	emphasizeNav($(this), false);
 },
 function() {
-	deEmphasizeNav($(this));
+	deEmphasizeNav($(this), false);
 });
 
 //Emphasize the currently viewed collection
@@ -34,10 +44,10 @@ $(window).scroll(function() {
 
 	$(".collection-item").each(function() {
 		if ($(this).attr('id').split('-')[0] === currentlyViewedCollection) {
-			emphasizeNav($(this));
+			emphasizeNav($(this), true);
 		}
 		else {
-			deEmphasizeNav($(this));
+			deEmphasizeNav($(this), true);
 		}
 	});
 });
