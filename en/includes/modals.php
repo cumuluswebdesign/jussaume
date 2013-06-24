6fa80d6425bbf4ec8e419762d67ef4c40f1d5757
Francis Marineau <frank.marineau@gmail.com>
2013-06-24 17:11:58 -0400

    Contact form

--- en/includes/modals.php
+++ en/includes/modals.php
@@ -4,13 +4,16 @@
 	<h3>Contact</h3>
 	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
   <form id="contact" method="post" action="">
-    <fieldset>    
-    <input type="text" name="name" placeholder="Name">
-    <input type="email" name="email" placeholder="Email">
-    <input type="text" name="company" placeholder="Company (Optional)">
-    <textarea name="message"></textarea>
+    <fieldset>
+    <div style="float: left; width: 340px;">
+      <input type="text" name="name" placeholder="Name">
+      <input type="email" name="email" placeholder="Email">
+      <input type="text" name="company" placeholder="Company (Optional)">
+    </div>
+
+    <textarea name="message" placeholder="Your message"></textarea>
  
-    <input type="submit" name="submit" id="submit" value="Send">
+    <input type="submit" value="Send">
  
     </fieldset>
   </form>
