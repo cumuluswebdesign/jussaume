52499bff73a41a773c39eb02f80dcf5d5c615912
Francis Marineau <frank.marineau@gmail.com>
2013-06-24 00:49:37 -0400

    Resize wrapper vertically with JS instead

--- js/landing.js
+++ js/landing.js
@@ -1,12 +1,21 @@
-//Vertical alignment of logo and links
-$('#landing-wrapper img').css('margin-top', ($(window).height()/2 - 400/2) + 'px');
+//Wrapper vertical size
+$('#landing-wrapper').height($(window).height() - 10);
 
 $(window).resize(function() {
-	$('#landing-wrapper img').css('margin-top', ($(window).height()/2 - 400/2) + 'px');
+	$('#landing-wrapper').height($(window).height() - 10);
+});
+
+//Vertical alignment of logo and links
+$('#landing-wrapper img').css('margin-top', ($(window).height()/2 - 390/2) + 'px');
+
+$(window).resize(function() {
+	$('#landing-wrapper img').css('margin-top', ($(window).height()/2 - 390/2) + 'px');
 });
 
 //Animations on link click
 $('#en-link, #fr-link').click(function() {
+	var clickedLink = $(this);
+
 	$('#border').height(60);
 	$('#main-logo, #language-select').fadeOut(1000);
 
@@ -18,6 +27,6 @@ $('#en-link, #fr-link').click(function() {
 		borderLeftColor: '#e2e2e2',
 		borderRightColor: '#e2e2e2'
 	}, 1000, 'swing', function() {
-		location.href = 'en';
+		location.href = clickedLink.attr('id').split('-')[0];
 	});
 });
