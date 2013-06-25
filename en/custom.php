73f6d5ce3aabc32f78112c065818a0a2c6442f9a
Alexander Haniotis <alexanderhaniotis@gmail.com>
2013-06-25 17:15:09 -0400

    Added custom page

---
+++ en/custom.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Mobilier Jussaume | Collections</title>
+		<meta name="description" content="">
+		
+		<?php include '../includes/head.php'; ?>
+	</head>
+	<body>
+		<?php $page = "collections"; ?>
+		<?php include 'includes/header.php'; ?>
+
+		<div class="wrapper">
+			<div class="container" style="margin-top: 247px;">
+				<div class="sections-nav">
+					
+					 <div class="section" id="hampton-nav">
+					 	<div class="section-image-container">
+							<img src="/img/collection01.png">
+						</div>
+
+						<p>Hampton</p>
+					</div>
+
+					<div class="section" id="manhattan-nav">
+						<div class="section-image-container">
+							<img src="/img/collection02.png">
+						</div>
+
+						<p>Manhattan</p>
+					</div>
+
+					<div class="section" id="paris-nav">
+						<div class="section-image-container">
+							<img src="/img/collection03.png">
+						</div>
+
+						<p>Paris</p>
+					</div>
+
+					<div class="section section-last" id="vancouver-nav">
+						<div class="section-image-container">
+							<img src="/img/collection04.png">
+						</div>
+
+						<p>Vancouver</p>
+					</div>
+
+				</div>
+
+				<div class="section-header collection-header" id="hampton-header"><h3>Hampton</h3></div>
+
+				<div class="item"><a href="#"><img src="/img/item/table01.jpg"></img></a></div>
+				<div class="item"><a href="#"><img src="/img/item/chair01.jpg"></img></a></div>
+				<div class="item"><a href="#"><img src="/img/item/chair02.jpg"></img></a></div>
+				<div class="item"><a href="#"><img src="/img/item/chair03.jpg"></img></a></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+
+				<div class="section-header collection-header" id="manhattan-header"><h3>Manhattan</h3></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+
+				<div class="section-header collection-header" id="paris-header"><h3>Paris</h3></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+
+				<div class="section-header collection-header" id="vancouver-header"><h3>Vancouver</h3></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+				<div class="item"></div>
+
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+				<div class="item last-row"></div>
+
+			</div>
+
+			<?php include 'includes/footer.php'; ?>
+		</div>
+
+		<?php include 'includes/modals.php'; ?>
+		<?php include 'includes/scripts.php'; ?>
+
+		<script src="/js/sections-nav.js"></script>
+	</body>
+</html>
