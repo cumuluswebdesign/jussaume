d6832404b83e755036fe58a5c1d4a9fe02f1b74f
Francis Marineau <frank.marineau@gmail.com>
2014-01-16 21:28:46 -0500

    Fix if in sendEmail

--- sendEmail.php
+++ sendEmail.php
@@ -1,50 +1,50 @@
 <?php
 
   try {
-    if (isset($_POST["email"]) AND isset($_POST["name"]) AND isset($_POST["company"]) AND isset($_POST["message"]))
+    if (isset($_POST["email"]) AND isset($_POST["name"]) AND isset($_POST["company"]) AND isset($_POST["message"])) {
+      header('Content-Type: application/json'); //Necessary for jQuery to understand we're sending JSON
 
-    header('Content-Type: application/json'); //Necessary for jQuery to understand we're sending JSON
+      require_once "lib/PHPMailer/class.phpmailer.php";
 
-    require_once "lib/PHPMailer/class.phpmailer.php";
+      $mail = new PHPMailer(true); //We pass true to throw exceptions
 
-    $mail = new PHPMailer(true); //We pass true to throw exceptions
+      $mail->IsSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->Port = 465;
+      $mail->SMTPAuth = true;
+      $mail->Username = 'hello@volume7.io';
+      $mail->Password = 'mariotis';
+      $mail->SMTPSecure = 'ssl';
 
-    $mail->IsSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 465;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'hello@volume7.io';
-    $mail->Password = 'mariotis';
-    $mail->SMTPSecure = 'ssl';
+      $mail->AddReplyTo($_POST["email"], $_POST["name"]);
+      $mail->From = $_POST["email"];
+      $mail->FromName = $_POST["name"];
+      $mail->AddAddress('louismartin.jussaume@gmail.com');
 
-    $mail->AddReplyTo($_POST["email"], $_POST["name"]);
-    $mail->From = $_POST["email"];
-    $mail->FromName = $_POST["name"];
-    $mail->AddAddress('louismartin.jussaume@gmail.com');
+      $mail->Subject = 'Message du site';
 
-    $mail->Subject = 'Message du site';
+      $mail->IsHTML(true);
+      $mail->Body = $_POST["message"];
 
-    $mail->IsHTML(true);
-    $mail->Body = $_POST["message"];
+      if (trim($_POST["company"]) != "") {
+        $mail->body .= "<hr>" . $_POST["company"];
+      }
 
-    if (trim($_POST["company"]) != "") {
-      $mail->body .= "<hr>" . $_POST["company"];
-    }
+      if(!$mail->Send()) {
+        $response = array(
+          'success' => false
+        );
 
-    if(!$mail->Send()) {
+        echo json_encode($response);
+        exit;
+      }
+
       $response = array(
-        'success' => false
+        'success' => true
       );
 
       echo json_encode($response);
-      exit;
     }
-
-    $response = array(
-      'success' => true
-    );
-
-    echo json_encode($response);
   }
   catch(Exception $e) {
     $response = array(
@@ -53,5 +53,4 @@
 
     echo json_encode($response);
   }
-
 ?>
